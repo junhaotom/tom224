e117c08572898145849bac5bdfabb680863673d0
TOM <31958805+junhaotom@users.noreply.github.com>
2017-10-20 12:40:36 -0400

    finish 5

---
+++ chapter3/3.py
@@ -0,0 +1,3 @@
+months = [ 'January', 'February','March','April','May','June','July','August','September','October','November','December']
+for i in months:
+    print(" one of the months of the years is ",i)
--- chapter4/3.py
+++
@@ -1,3 +0,0 @@
-months = (None, 'January', 'February', etc)
-for y in range(1, 13):
-    m = months[y] 
